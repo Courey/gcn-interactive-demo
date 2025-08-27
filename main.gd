6650c6d51034d06baa11d501b979481faaef076b
dakota002 <dakota.d@live.com>
2025-08-27 13:41:33 -0400

    Move files, add globals, track player
    Improve score tracking
    
    Make events more dynamic
    
    Fix player light cone, fix event scale

--- main.gd
+++ main.gd
@@ -21,32 +21,35 @@ const OFFSET_MAP = [
 var target_map:Dictionary = {}
 
 func _ready():
-	randomize() 
+	randomize()
 
 	for player in Players.get_children():
 		player.initialize_with_set_values()
-		player.connect("observe",_on_player_observe)
 		target_map[player.name] = []
-		
-	RotatingStarField.ROTATION_NODE_POSITION = OFFSET_MAP[SELECTED_OFFSET]
+		player.connect("observe", _on_player_observe)
+
+	Global.ROTATION_AXIS = OFFSET_MAP[SELECTED_OFFSET]
 	RotatingStarField.initialize_with_set_values()
 
-	Events.position = OFFSET_MAP[SELECTED_OFFSET]
+	Events.position = Global.ROTATION_AXIS
 
 	for i in range(EVENTS_LIMIT):
 		create_new_event()
-	
-	
+
+
 func _process(delta: float) -> void:
-	Events.rotate(SPEED * delta)
+	var rotation_speed = SPEED * delta
+	Events.rotate(rotation_speed)
+	rotate_observing_players(rotation_speed)
 	ScoreLabel.text = "Score: " + str(score)
 	if (len(Events.get_children()) < EVENTS_LIMIT):
 		create_new_event()
-	
-	
+
+
 func create_new_event():
 	var newEvent = Event.instantiate()
 	newEvent.set("location", get_random_point())
+	newEvent.set("points", randi_range(10,20))
 	Events.add_child(newEvent)
 	newEvent.connect('player_over', _on_event_player_over)
 	newEvent.connect('player_exited', _on_player_exited)
@@ -56,9 +59,9 @@ func get_random_point() -> Vector2:
 	var center_vector = get_viewport_rect().size / 2. # Center of screen
 	var angle = randf_range(0, TAU)
 	var radius = center_vector.y
-	var distance = randf() * radius 
+	var distance = randf() * radius
 	return center_vector + Vector2.from_angle(angle) * distance
-	
+
 
 func _on_event_player_over(event:Node2D, player:Node2D) -> void:
 	target_map[player.name].append(event)
@@ -66,9 +69,15 @@ func _on_event_player_over(event:Node2D, player:Node2D) -> void:
 
 func _on_player_exited(event:Node2D, player:Node2D) -> void:
 	target_map[player.name].remove_at(target_map[player.name].find(event))
-	
 
-func _on_player_observe(player:Node2D) -> void:
+
+func _on_player_observe(player:Player):
 	for event in target_map[player.name]:
-		score += event.points
-		event.queue_free()
+		score += player.sensitivity
+		#event.points -= player.sensitivity
+	player.sensitivity_timer.start()
+
+func rotate_observing_players(rotation_speed):
+	for player in Players.get_children() as  Array[Player]:
+		if player.is_observing:
+			player.rotate_relative_to_background(rotation_speed)
--- player.gd
+++ player.gd
@@ -1,81 +1,109 @@
-extends Node2D
+class_name Player extends Node2D
 
 @export var input_prefix := "p1"
 @export var active = false
-@export var target_speed := 300.0
-@export var observation_radius = 100
 @export var player_color:Color
 @export var start_position:Vector2 = Vector2.ZERO
+
+# "Player Stats:
+@export var slew_speed := 300.0
+@export var sensitivity := 1 # Points drained from events per second of observation
+@export var observation_area = Vector2.ZERO
+
 
 @onready var target = $Target
 @onready var light_cone = $LightCone
 @onready var collision_shape = $Target/TargetFocus/CollisionShape2D
-@onready var coneShader = $Target/TargetFocus/CollisionShape2D.material as ShaderMaterial
-@onready var timer = $InactiveTimeout
+@onready var target_shader = $Target/TargetFocus/CollisionShape2D.material as ShaderMaterial
+@onready var timer = $InactiveTimeout as Timer
+@onready var sensitivity_timer = $SensitivityTimer as Timer
+
 
 signal observe
+signal abort
+
+var is_observing = false
 
 func _ready():
 	position = start_position
 	player_color = player_color if player_color else Color(randf(), randf(), randf(), .5)
-	collision_shape.shape.radius = observation_radius
-	coneShader.set_shader_parameter('circle_color', player_color) 
-	
+	target_shader.set_shader_parameter('player_color', player_color)
+
+	collision_shape.shape.size = observation_area
+
 	light_cone.polygon = PackedVector2Array([
 		Vector2.ZERO,
-		Vector2(collision_shape.shape.radius, collision_shape.position.y),
-		Vector2(-collision_shape.shape.radius, collision_shape.position.y),
+		Vector2.ZERO,
+		Vector2.ZERO,
 	])
 	light_cone.color = player_color
+	light_cone.color.a = .5
 
-func _input(event):	
+	sensitivity_timer.wait_time = sensitivity
+
+
+
+func initialize_with_set_values(): # Do we need this?
+	pass
+
+
+func _input(event):
 	if (event.is_action_pressed(input_prefix + "_select")):
 		# Enable player
 		if (!active):
 			active = true
 			light_cone.modulate.a = .5
+		elif is_observing:
+			is_observing = false
+			sensitivity_timer.stop()
+			abort.emit(self)
 		else:
-			observe.emit(self)
-		
-	
+			is_observing = true
+			sensitivity_timer.start()
+			#observe.emit(self)
+		target_shader.set_shader_parameter('is_observing', is_observing)
+
+
 	# Restart the timeout counter for inactive players
 	timer.stop()
 	timer.start()
+
 
 func _process(delta):
 	if (!active):
 		light_cone.modulate.a = .2
 		return
-	
-	
-	var move_input = Vector2(
-		Input.get_action_strength(input_prefix + "_right") 
-		- Input.get_action_strength(input_prefix + "_left"),
-		Input.get_action_strength(input_prefix + "_down") 
-		- Input.get_action_strength(input_prefix + "_up")
-	)
 
-	# Move target relative to player position
-	var new_target_pos:Vector2 = target.position + move_input * target_speed * delta
-	target.position = new_target_pos
+	if !is_observing:
+		var move_input = Vector2(
+			Input.get_action_strength(input_prefix + "_right")
+			- Input.get_action_strength(input_prefix + "_left"),
+			Input.get_action_strength(input_prefix + "_down")
+			- Input.get_action_strength(input_prefix + "_up")
+		)
 
-	new_target_pos.length()
+		# Move target relative to player position
+		var new_target_pos:Vector2 = target.position + move_input * slew_speed * delta
+		target.position = new_target_pos
+
 	light_cone.polygon = draw_arms()
-	
+	#light_cone.rotation = rotation
 
-func draw_arms() -> PackedVector2Array:
+
+
+func draw_arms_circular() -> PackedVector2Array:
 	var shape = collision_shape.shape as CircleShape2D
 
 	var target_position = collision_shape.global_position - global_position
-	var target_angle = target_position.angle() - collision_shape.global_rotation 
+	var target_angle = target_position.angle() - collision_shape.global_rotation
 	var T = target_position.length()
 	var angle_offset = asin(shape.radius/T)
-	
+
 	var phi_1 = target_angle + angle_offset
 	var phi_2 = target_angle - angle_offset
-	
+
 	var l = sqrt(T**2 - (shape.radius)**2)
-	
+
 	return PackedVector2Array([
 		Vector2.ZERO,
 		Vector2(cos(phi_1) * l, sin(phi_1)*l),
@@ -83,7 +111,35 @@ func draw_arms() -> PackedVector2Array:
 	])
 
 
+func draw_arms() -> PackedVector2Array:
+	var shape = collision_shape.shape as RectangleShape2D
+	var target_position:Vector2 = Vector2(
+		collision_shape.global_position - global_position
+	).rotated(-rotation)
+
+	return PackedVector2Array([
+		# Point at the players origin
+		Vector2.ZERO,
+		# Bottom left corner:
+		target_position + Vector2(-shape.size.x/2, shape.size.y/2),
+		# Bottom right corner
+		target_position + Vector2(shape.size.x/2, shape.size.y/2),
+	])
+
+
+func rotate_relative_to_background(rotation_speed) -> void:
+	var relative_vector:Vector2 = collision_shape.global_position
+	relative_vector -= Global.ROTATION_AXIS
+	relative_vector = relative_vector.rotated(rotation_speed)
+	relative_vector += Global.ROTATION_AXIS
+	collision_shape.global_position = relative_vector
+
+
 func _on_inactive_timeout_timeout() -> void:
 	active = false
 	target.global_position = start_position
 	light_cone.polygon = draw_arms()
+
+
+func _on_sensitivity_timer_timeout() -> void:
+	observe.emit(self)
